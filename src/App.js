91b72c9065b8df4b502a1e17f6e16789c47124d8
Yash Mahale <yashmahale1702@gmail.com>
2022-08-17 00:27:32 +0530

    module 1 complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.scss';
+import { Expenses } from './components/Expenses/Expenses';
+import { expenses } from './data/Data';
 
 function App() {
-    return <div className='App'>Home Page</div>;
+    return (
+        <div className='App'>
+            <Expenses items={expenses}></Expenses>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from '../UI/Card';
+import './ExpenseItem.scss';
+
+export const ExpenseItem = (props) => {
+    const month = props.date.toLocaleString('en-US', { month: 'long' });
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
+    return (
+        <Card className='expense-item'>
+            <div className='expense-date'>
+                <div className='expense-date__month'>{month}</div>
+                <div className='expense-date__year'>{year}</div>
+                <div className='expense-date__day'>{day}</div>
+            </div>
+            <div className='expense-item__description'>
+                <h2> {props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+        </Card>
+    );
+};
